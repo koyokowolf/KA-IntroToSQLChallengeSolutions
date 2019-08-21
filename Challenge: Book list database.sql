75de827e84a742ffeb3211a709ce2728368bd516
Koyoko Wolf <54044163+koyokowolf@users.noreply.github.com>
2019-08-20 20:19:31 -0700

    Create Challenge: Book list database.sql

---
+++ Challenge: Book list database.sql
@@ -0,0 +1,9 @@
+/*Step 1: Create a table to store your list of books. It should have columns for id, name, and rating.*/
+
+CREATE TABLE Favorite_Books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+/* Step 2: Add three of your favorite books into the table. (Make it your own DON'T USE MINE)*/
+
+INSERT INTO Favorite_Books VALUES (1, "NATIVE WAYS", 5);
+INSERT INTO Favorite_Books VALUES (2, "INDIAN HERBALOGY", 4);
+INSERT INTO Favorite_Books VALUES (3, "RED ROAD", 3);
